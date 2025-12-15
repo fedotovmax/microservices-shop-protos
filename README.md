2131c7952bbb0012b5c7076eb175a244c038f144
Maxim Fedotov <maximfedotov@MacBook-Air-M4.local>
2025-12-15 16:01:44 +0500

    add notification payload to events contracts

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Download
 
-go get github.com/fedotovmax/microservices-shop-protos@v1.0.9
+go get github.com/fedotovmax/microservices-shop-protos@v1.0.10
 
 ## Metadata
 
--- events/notifications.go
+++ events/notifications.go
@@ -5,3 +5,10 @@ const NOTIFICATIONS_EVENTS = "notifications.events"
 const NOTIFICATIONS_EMAIL = "notifications.email"
 
 const NOTIFICATIONS_TELEGRAM = "notifications.telegram"
+
+type EmailVerifyNotificationPayload struct {
+	Email       string
+	Title       string
+	Description string
+	Link        string
+}
