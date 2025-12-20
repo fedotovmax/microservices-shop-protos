4088ccd4921cd34479b918cb16b6c45f6c678b9e
Maxim Fedotov <maximfedotov@MacBook-Air-M4.local>
2025-12-20 12:12:49 +0500

    remove notifications events, will be listen domain events

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Download
 
-go get github.com/fedotovmax/microservices-shop-protos@v1.0.12
+go get github.com/fedotovmax/microservices-shop-protos@v1.0.13
 
 ## Metadata
 
--- events/user.go
+++ events/user.go
@@ -6,12 +6,15 @@ const USER_CREATED = "user.created"
 
 const USER_DELETED = "user.deleted"
 
-const USER_UPDATED = "user.updated"
+const USER_PROFILE_UPDATED = "user.profile.updated"
 
 type UserCreatedEventPayload struct {
-	ID string `json:"id"`
+	ID     string `json:"id"`
+	Email  string `json:"email"`
+	Locale string `json:"locale"`
 }
 
 type UserUpdatedEventPayload struct {
-	ID string `json:"id"`
+	ID     string `json:"id"`
+	Locale string `json:"locale"`
 }
