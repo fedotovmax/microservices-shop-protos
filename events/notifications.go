ad10e5501874e4a6d7a72409bfce259ce1e593bc
Maxim Fedotov <maximfedotov@MacBook-Air-M4.local>
2025-12-20 08:46:34 +0500

    add telegram notification payload

--- events/notifications.go
+++ events/notifications.go
@@ -7,9 +7,14 @@ const NOTIFICATIONS_EMAIL = "notifications.email"
 const NOTIFICATIONS_TELEGRAM = "notifications.telegram"
 
 type EmailVerifyNotificationPayload struct {
-	Email       string
-	Title       string
-	Description string
-	Link        string
-	Locale      string
+	Email       string `json:"email"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Link        string `json:"link"`
+	Locale      string `json:"locale"`
+}
+
+type TelegramNotificationPayload struct {
+	UserID string `json:"user_id"`
+	Text   string `json:"text"`
 }
