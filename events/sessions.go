0530aba08d0634858171c2873119b1f27792a106
fedotovmax <f3d0t0vmax@yandex.ru>
2025-12-30 12:14:48 +0500

    add bypass ip login event

--- events/sessions.go
+++ events/sessions.go
@@ -6,9 +6,18 @@ const SESSION_EVENTS = "session.events"
 
 const SESSION_BLACKLIST_ADDED = "session.blacklist.added"
 
+const SESSION_BYPASS_ADDED = "session.bypass.added"
+
 type SessionBlacklistAddedEventPayload struct {
 	UID           string    `json:"uid"`
 	Email         string    `json:"email"`
 	Code          string    `json:"code"`
 	CodeExpiresAt time.Time `json:"code_expires_at"`
+}
+
+type SessionBypassAddedEventPayload struct {
+	UID             string    `json:"uid"`
+	Email           string    `json:"email"`
+	Code            string    `json:"code"`
+	BypassExpiresAt time.Time `json:"code_expires_at"`
 }
