6cb50b28b24505e780fb5cb3fb77d4ea1686cb97
fedotovmax <f3d0t0vmax@yandex.ru>
2026-01-26 09:58:37 +0500

    add USER_EMAIL_VERIFY_LINK_ADDED event

--- events/user.go
+++ events/user.go
@@ -6,16 +6,24 @@ const USER_EVENTS = "user.events"
 
 const USER_CREATED = "user.created"
 
+const USER_EMAIL_VERIFY_LINK_ADDED = "user.verify-email-link.added"
+
 const USER_DELETED = "user.deleted"
 
 const USER_PROFILE_UPDATED = "user.profile.updated"
 
-type UserCreatedEventPayload struct {
+type UserEmailVerifyLinkAdded struct {
 	ID                       string    `json:"id"`
 	Email                    string    `json:"email"`
 	EmailVerifyLink          string    `json:"email_verify_link"`
 	EmailVerifyLinkExpiresAt time.Time `json:"email_verify_link_expires_at"`
 	Locale                   string    `json:"locale"`
+}
+
+type UserCreatedEventPayload struct {
+	ID     string `json:"id"`
+	Email  string `json:"email"`
+	Locale string `json:"locale"`
 }
 
 type UserProfileUpdatedEventPayload struct {
